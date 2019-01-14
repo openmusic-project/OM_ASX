6b974ba1e92923c5b70a482975b83d7bf3c3884d
j-bresson <bresson@ircam.fr>
2019-01-14 20:56:01 +0100

    import

---
+++ sources/OM-ASX-clipping.lisp
@@ -0,0 +1,1 @@
+;****************************;    OM-functions by Hans Tutschku april 2005(in-package ASX);************(om::defmethod! ASX::clip-par  ((vals list) (filename string))   :indoc '("list")  :icon 999  :doc""    ;(ASX::save-or-not vals filename)vals)
